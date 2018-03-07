bc7b3dc76a2e9b333f849f979ff8ca3c12c3d15a
Jessica McAloon <mcaloonj@bu.edu>
2018-03-06 19:57:24 -0500

    works for all cases

--- main.js
+++ main.js
@@ -7,18 +7,52 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            // if (text === "Stress"){
-            //   console.log(text.substr(text.length-3));
-            // }
-            if (text.substr(text.length-3) === "ess"){
-              var newText = text.concat("ica");
-            }
+            let text = node.nodeValue;
+            let words = text.split(" ");
 
-            if (newText !== undefined && newText !== text){
-              element.replaceChild(document.createTextNode(newText), node);
+          for (let i = 0; i < words.length; i++){
+            let word = words[i];
+            let rgx = new RegExp('(?!essence)(?!essional)ess');
+            //let rgx = new RegExp('ess(?:es|ed|ion)?');
+            if (rgx.test(word)){
+              let newWord = "";
 
+              let rgx2 = new RegExp('[.,\/#!$|%\^&\*;:{}=\-_`~()]$');
+              let hasPunc = false;
+              let punc = "";
+
+              if (rgx2.test(word)){
+                hasPunc = true;
+                punc = word.slice(-1);
+                word = word.slice(0,-1);
+              }
+              if (word.slice(-2) === "es"){
+                newWord = word.slice(0,-2) + "icas";
+              }
+              else if (word.slice(-2) === "ed"){
+                newWord = word.slice(0,-2) + "ica";
+              }
+              else if (word.slice(-1) === "e"){ //for words like "finesse"
+                newWord = word.slice(0,-1) + "ica";
+              }
+              else if (word.slice(-3) === "ion" || word.slice(-3) === "ive"|| word.slice(-3) === "ing"){
+                newWord = word.slice(0,-3) + "ica";
+              }
+              else if (word.slice(-4) === "ings"){
+                newWord = word.slice(0,-4) + "icas";
+              }
+              else{
+                newWord = word.concat("ica");
+              }
+
+              if (hasPunc){
+                newWord = newWord.concat(punc);
+              }
+              words[i] = newWord;
             }
+          }
+          let newText = words.join(" ");
+          element.replaceChild(document.createTextNode(newText), node);
         }
     }
 }
