6d1316425e9b10eee3efef2d29bbd7e6293b405c
Jessica McAloon <mcaloonj@bu.edu>
2018-03-11 20:15:38 -0400

    works for hyphens and URLs

--- main.js
+++ main.js
@@ -13,29 +13,37 @@ for (var i = 0; i < elements.length; i++) {
           for (let x = 0; x < words.length; x++){
             let word = words[x];
             let blacklist = ['session', 'sessions', 'jess', 'jesse'];
-            if (word.match("ess(?!en)(?!ex)(?!ica)(?!fully)(?!ure)(?!or)(?!on)(?!a)(?!y)(?!p)(?!ional)(?!ie)(?!ible)(?!ively)(?!ibility)") && word.indexOf("-") === -1 && blacklist.indexOf(word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")) === -1 ){
-              let idx = word.lastIndexOf("ess") + 3;
-              let newWord = [word.slice(0,idx), "ica", word.slice(idx)].join('');
+            if (word.match("ess(?!en)(?!ex)(?!ica)(?!fully)(?!ure)(?!or)(?!on)(?!a)(?!y)(?!p)(?!ional)(?!ie)(?!ible)(?!ively)(?!ibility)(?!ment)") && blacklist.indexOf(word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")) === -1 ){
+              let lst = word.split(/(?=[^a-z])/ig); //need to split word on punctuation in case of hyphens or URLs
+              for (var y = 0; y < lst.length; y++){
+                let subWord = lst[y];
+                console.log(subWord);
+                if (subWord.match("ess")){
+                  let idx = subWord.lastIndexOf("ess") + 3;
+                  let newWord = [subWord.slice(0,idx), "ica", subWord.slice(idx)].join('');
+                  console.log(newWord)
+                  if (newWord.lastIndexOf("icaions") !== -1){
+                    idx = newWord.lastIndexOf("icaions") + 3;
+                    newWord = newWord.slice(0,idx) + "s";
+                  }
 
-              if (newWord.lastIndexOf("icaions") !== -1){
-                let idx = newWord.lastIndexOf("icaions") + 3;
-                newWord = newWord.slice(0,idx) + "s";
+                  else if (newWord.lastIndexOf("icaes") !== -1 ){
+                    idx = newWord.lastIndexOf("icaes") + 3;
+                    newWord = newWord.slice(0,idx) + "s";
+                  }
+
+                  else if (newWord.lastIndexOf("icaings") !== -1){
+                    idx = newWord.lastIndexOf("icaings") + 3;
+                    newWord = newWord.slice(0,idx) + "s";
+                  }
+
+                  idx = newWord.lastIndexOf("ica")+3;
+                  let end = newWord.slice(idx).replace(/[a-r&&t-z]/g, ""); //replace any letter that isn't s (keep plural)
+                  newWord = newWord.slice(0,idx) + end;
+                  lst[y]  = newWord;
+                }
               }
-
-              else if (newWord.lastIndexOf("icaes") !== -1 ){
-                let idx = newWord.lastIndexOf("icaes") + 3;
-                newWord = newWord.slice(0,idx) + "s";
-              }
-
-              else if (newWord.lastIndexOf("icaings") !== -1){
-                let idx = newWord.lastIndexOf("icaings") + 3;
-                newWord = newWord.slice(0,idx) + "s";
-              }
-
-              idx = newWord.lastIndexOf("ica")+3;
-              let end = newWord.slice(idx).replace(/[a-r&&t-z]/g, "");
-              newWord = newWord.slice(0,idx) + end;
-
+              newWord = lst.join("");
               words[x] = newWord;
             }
           }
